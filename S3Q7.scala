d4da2398d87a15e31dffe873a6dd27d69a2f3f4f
Henry Pearson <pearsonhenryj@icloud.com>
2021-02-19 15:59:27 +0000

    added test for Q7

--- S3Q7.scala
+++ S3Q7.scala
@@ -2,22 +2,63 @@ import io.threadcso._
 
 class resourceMonitor {
 
-    // number of threads accessing the resource
-    private var active = 0
-
     // total of the ids of processes that are accessing the resource
     private var total = 0
 
     def enter(id: Int) = synchronized {
         while ((total % 3) != 0) wait()
-        active += 1
         total += id
     }
 
     def exit(id: Int) {
-        active -= 1
         total -= id
         notifyAll()
     }
 }
 
+import ox.cads.testing._
+import scala.collection.immutable.Set
+
+object resourceMonitorTest {
+
+
+
+    // functions for sequential implementation
+    // precondition that the sum of ids must be a multiple of 3
+    def seqEnter(me: Int)(set: Set[Int]) : (Unit, Set[Int]) = {
+        require(set.sum % 3 == 0)
+        idTotal += me
+        ((), set.incl(me))
+    }
+
+    def seqExit(me: Int)(set: Set[Int]) : (Unit, Set[Int]) = {
+        idTotal -= me
+        ((), set.excl(me))
+    }
+
+    def reader(me: Int, log: GenericThreadLog[Set[Int], resourceMonitor]) = proc {
+        for (i <- 0 until 200) {
+            // attempt to access the resource
+            log.log(_.enter(me), "Entering", seqEnter(me))
+            // attempt to exit accessing the resource
+            log.log(_.exit(me), "Exiting", seqExit(me))
+        }
+    }
+
+    def doTest = {
+        // concurrent resource monitor object
+        val concDatabase = new resourceMonitor()
+        // set to store which processes are active for sequential part of test
+        val seqDatabase = new Set[Int]()
+        val tester = LinearizabilityTester.JITGraph[Set[Int], resourceMonitor](seqDatabase, concDatabase, 5, reader _, 400)
+        assert(test() > 0)
+    }
+
+    def main(args: Array[String]) = {
+        for(i <- 0 until 1000){
+            doTest
+            if(i%10 == 0) print(".")
+        }
+        println; io.threadcso.exit
+    }
+}
